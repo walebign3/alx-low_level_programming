b24c95dc0a31f79d980dd6f030b298b587656ac7
walebign3 <walebign3@gmail.com>
2022-04-07 16:47:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-/*First c 
+#include<stdio.h>
+
+/**
+ * main - main block
  *
- *program*/
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
