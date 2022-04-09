62c8fa0f1289d7e7d569e912df23dee999d01706
walebign3 <walebign3@gmail.com>
2022-04-08 18:23:38 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - THe entry point of the program
+*
+* Return: The main function prints the alphabet
+*/
+int main(void)
+{
+	int ch, sec;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (sec = 'A'; sec <= 'Z'; sec++)
+	{
+		putchar(sec);
+	}
+	putchar('\n');
+	return (0);
+}
+© 2022 GitHub, Inc.
