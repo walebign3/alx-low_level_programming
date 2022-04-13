a87cc86d8255652c2bd1923271771fa5913e8edf
walebign3 <walebign3@gmail.com>
2022-04-13 10:16:54 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value
+ * Description: print_absolute value.
+ * Return: absolute value.
+ */
+int print_last_digit(int n)
+{
+	n=n%10;
+	return (n);
+}
