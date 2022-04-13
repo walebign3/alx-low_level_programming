b79553cec05c37ebfb80bf70178aa462c7303498
walebign3 <walebign3@gmail.com>
2022-04-13 00:39:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *	Description: Alphabet list function.
+ *  Return: lower case alphabet list
+ */
+void print_alphabet()
+{
+	int n;
+	for (n='a'; n<='z'; n++)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,2 @@
-/* this prototype has to be declared in a header file */
 int _putchar(char n);
+void print_alphabet(void);
