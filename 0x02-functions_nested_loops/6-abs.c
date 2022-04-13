6ed1a3a99441b1c3213960b47ac90604e99365c7
walebign3 <walebign3@gmail.com>
2022-04-13 10:06:06 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value
+ * Description: print_absolute value.
+ * Return: absolute value.
+ */
+int _abs(int n)
+{
+	if (n < 1)
+	{
+		n = -n;
+		return (n);
+	}
+	else 
+		return (n);
+}
