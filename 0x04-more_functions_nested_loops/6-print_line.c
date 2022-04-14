7d7b36c72f5cb9b19afa1fc50f94518a1ea3473f
walebign3 <walebign3@gmail.com>
2022-04-14 20:37:45 +0300

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,7 +11,9 @@ void print_line(int n)
 	int i;
 
 	if (n > 0)
-		for (i = 0; i <= n; i++)
+	{	for (i = 0; i <= n; i++)
 			_putchar(95);
-	_putchar('\n');
+		_putchar('\n');
+	}
+	else _putchar('\n');
 }
