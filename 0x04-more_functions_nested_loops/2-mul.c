edd6f196b49152eded39dbff68e9df6124b58055
walebign3 <walebign3@gmail.com>
2022-04-14 13:51:01 +0300

    2-main.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - print multiplication a and b
+ * @c: identify character
+ * Description: print multiplication result value.
+ * Return: return multiplication result.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
