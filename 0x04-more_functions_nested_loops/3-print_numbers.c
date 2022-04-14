c7f2db10404b729197e58d83265129eb70386f90
walebign3 <walebign3@gmail.com>
2022-04-14 15:23:21 +0300

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,13 @@
 
 /**
  * print_numbers - print numbers 0-9
+ *
  */
 void print_numbers(void)
 {
-	for (int n = 48; n <= 57; n++)
+	int n;
+	
+	for (n = 48; n <= 57; n++)
 		_putchar(n);
 	_putchar('\n');
 }
