fbc5352eab82c202c8526a5646e0a3b7f8d3f09a
walebign3 <walebign3@gmail.com>
2022-04-14 13:24:57 +0300

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - print absolute value
+ * @c: identify uppercase character
+ * Description: print_absolute value.
+ * Return: absolute value.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
