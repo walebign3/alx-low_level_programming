345d41e5454c56bd7f4d6d9a153e3ffd92550f72
walebign3 <walebign3@gmail.com>
2022-04-19 19:53:36 +0300

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,10 @@ void puts2(char *str)
 {
 	int n = 0;
 
-	while (*(str+n) != '\0')
+	while (*(str + n) != '\0')
 	{
-		if (n%2 == 0)
-			_putchar(*(str+n));
+		if (n % 2 == 0)
+			_putchar(*(str + n));
 		n++;
 	}
 	_putchar('\n');
