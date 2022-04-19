0f44126d7d836b4213431526b94e556591d87671
walebign3 <walebign3@gmail.com>
2022-04-19 20:37:57 +0300

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - string display
+ * @str: pointer to char
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+	int m = 0;
+
+	while (*(str + n) != '\0')
+	{
+		n++;
+	}
+	m = n / 2;
+	while (*(str + m) != '\0')
+	{
+		_putchar(*(str + m));
+		m++;
+	}
+	_putchar('\n');
+}
