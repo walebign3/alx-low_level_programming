70f3f53bd57c0daa3024c8ca17a149bf1fb75a7b
walebign3 <walebign3@gmail.com>
2022-04-19 19:49:07 +0300

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - string display
+ * @str: pointer to char
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int n = 0;
+
+	while (*(str+n) != '\0')
+	{
+		if (n%2 == 0)
+			_putchar(*(str+n));
+		n++;
+	}
+	_putchar('\n');
+}
