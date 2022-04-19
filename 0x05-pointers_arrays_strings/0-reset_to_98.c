024e626f7e3238bf8101fd977131b0169ca689d9
walebign3 <walebign3@gmail.com>
2022-04-19 13:59:40 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - print numbers 0-9 except 2 and 4
+ * @n: pointer numbers
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p=n;
+	*p=48;
+}
