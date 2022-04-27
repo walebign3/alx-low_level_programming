368742be1f0fe693a60e11d90724d300eccb67f8
walebign3 <walebign3@gmail.com>
2022-04-28 02:31:42 +0300

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print numbers 0-9 except 2 and 4
+ * @s: pointer
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+        if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+        _puts_recursion(s+1);
+}
