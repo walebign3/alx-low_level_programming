0b0a1f3a1567a1a7370075dd9cd22b28822e265d
walebign3 <walebign3@gmail.com>
2022-04-28 12:07:33 +0300

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print numbers 0-9 except 2 and 4
+ * @s: pointer
+ *
+ * Return: void
+ */
+int n = 0;
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
