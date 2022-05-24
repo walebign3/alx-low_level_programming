0c0352831581521bee62059155356ba5f8e3eb54
walebign3 <walebign3@gmail.com>
2022-05-24 14:37:31 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint return number of nodes
- * @x - pointer to struct
+ * print_listint - return number of nodes
+ * @x: pointer to struct
  *
  * return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t x = 0;
