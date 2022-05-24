6c686f431d5eb87212d99d8f73b90f1d6653dcbd
walebign3 <walebign3@gmail.com>
2022-05-24 19:54:16 +0300

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,47 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - delete node
- * @head: pointer 1
- * @index: pointer 2
+ * delete_nodeint_at_index - deletes nodes at index
+ * @head: first pointer to node
+ * @index: index
  *
- * Return: 1 if succeed & -1 if failed
+ * Return: address of nodes
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int x;
 	listint_t *ptr1;
 	listint_t *ptr2;
-	unsigned int x = 1;
 
-	ptr1 = *head;
-	ptr2 = *head;
 	if (head == NULL || *head == NULL)
-	{
-		free (*head);
 		return (-1);
-	}
-	if (ptr1->next == NULL)
-	{
-		return (-1);
-	}
 	if (index == 0)
 	{
-		ptr1 = *head;
-		*head = ptr1->next;
-		free(ptr1);
+		ptr2 = (*head)->next;
+		free(*head);
+		*head = ptr2;
+		return (1);
 	}
-	else
+	ptr1 = *head;
+	for (x = 0; x < index - 1; x++)
 	{
-	while(index > x)
-		{
-			ptr2 = ptr1;
-			ptr1 = ptr1->next;
-			x++;
-		}
-		ptr2->next = ptr1->next;
-		free(ptr1);
+		if (ptr1->next == NULL)
+			return (-1);
+		ptr1 = ptr1->next;
 	}
+	ptr2 = ptr1->next;
+	ptr1->next = ptr2->next;
+	free(next);
 	return (1);
 }
