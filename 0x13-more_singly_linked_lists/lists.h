849fb09ffabae409d8d797574187d94881412978
walebign3 <walebign3@gmail.com>
2022-05-24 14:24:26 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef NODE_H
+#define NODE_H
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -23,3 +26,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+
+#endif
+
