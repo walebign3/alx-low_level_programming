caee7d9702bd30fe34ab0d1c78cd32ec0f99f155
walebign3 <walebign3@gmail.com>
2022-05-25 19:28:18 +0300

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints before main
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
