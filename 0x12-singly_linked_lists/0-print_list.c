a055505ae93ae593037eb9812b18aa0379e13987
walebign3 <walebign3@gmail.com>
2022-05-25 20:01:57 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,23 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *ptr;
-	unsigned int x = 0;
 
-	ptr = h;
-	while (ptr)
+	unsigned int x = 0;
+	const list_t *ptr = h;	
+	while (ptr != NULL)
 	{
-		x++;
-		printf("[%u] %s\n", ptr->len, ptr->str);
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+
+		}
 		ptr = ptr->next;
+		x++;
 	}
+
 	return (x);
 }
