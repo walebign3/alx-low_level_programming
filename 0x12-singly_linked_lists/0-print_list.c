66da17837e64107ddcfd1be90c7912031c21a1a0
walebign3 <walebign3@gmail.com>
2022-05-25 19:45:05 +0300

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print list
+ * @h: pointer to first node
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr;
+	unsigned int x = 0;
+
+	ptr = h;
+	while (ptr)
+	{
+		x++;
+		printf("[%u] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+	}
+	return (x);
+}
