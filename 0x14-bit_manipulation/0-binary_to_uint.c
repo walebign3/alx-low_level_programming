784d089b4efc98a307cdfbcaaee02149e80f01ad
walebign3 <walebign3@gmail.com>
2022-05-27 02:54:37 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i = 0, k = 1;
 	int len = 0, c;
 
-	if(b == NULL)
+	if (b == NULL)
 		return (0);
-	while(b[len] != '\0')
+	while (b[len] != '\0')
 		len++;
-	for(c = len-1; c >= 0; c--)
+	for (c = len-1; c >= 0; c--)
 	{
-		if(b[c] != '0' && b[c] != '1')
+		if (b[c] != '0' && b[c] != '1')
 			return (0);
-		if(b[c] == '1')
+		if (b[c] == '1')
 			i += k;
 		k *= 2;
 	}
