0b374dba73120929ec71622c54425a2cbf49b76a
walebign3 <walebign3@gmail.com>
2022-05-31 13:46:17 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,17 +23,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	BUF = malloc(sizeof(char) * letters);
 	if(BUF == NULL)
 		return (0);
-	if (filename == NULL)
+	if(filename == NULL)
 	{
 		return (0);
 	}
-	fd = open (filename, O_RDONLY);
-	if (fd == -1)
+	fd = open(filename, O_RDONLY);
+	if(fd == -1)
 		return (0);
-	read (fd, BUF, letters);
-	while (BUF[i])
+	read(fd, BUF, letters);
+	while(BUF[i])
 		i++;
-	wrt = write (1, BUF, i);
+	wrt = write(1, BUF, i);
 	if (wrt < 0)
 		return (0);
 	return (wrt);
