a62725e0c182cd7f2e82621220e928d04dedfb7c
walebign3 <walebign3@gmail.com>
2022-05-31 16:06:02 +0300

    0-read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	rd = read(fd, BUF, letters);
 	if(rd == -1)
 		return (0);
-	wrt = write(1, BUF, rd);
+	wrt = write(STDOUT_FILENO, BUF, rd);
 	if(wrt == -1 || wrt < rd)
 		return (0);
 	wrt = close(fd);
