4295fcf19cb678987bea86b0947ab1f6319ac699
walebign3 <walebign3@gmail.com>
2022-06-01 14:55:05 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, fd1, fd2, fd3;
 	ssize_t i = 0;
 	ssize_t wrt;
 
@@ -22,27 +22,30 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1 && text_content != NULL)
 	{
-		fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-		if (fd == -1)
+		fd1 = open(filename, O_CREAT | O_RDWR, 0600);
+		if (fd1 == -1)
 			return -1;
-		wrt = write(fd, text_content, i);
+		wrt = write(fd1, text_content, i);
 		if (wrt == -1)
                         return -1;
+		close(fd1);
 	}
 	else if (fd == -1 && text_content == NULL)
 	{
-		fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-		if (fd == -1)
+		fd2 = open(filename, O_CREAT | O_RDWR, 0600);
+		if (fd2 == -1)
                         return -1;
+		close(fd2);
 	}
 	else
 	{
-		fd = open(filename, O_TRUNC);
-		if (fd == -1)
+		fd3 = open(filename, O_TRUNC);
+		if (fd3 == -1)
                         return -1;
-		wrt = write(fd, text_content, i);
+		wrt = write(fd3, text_content, i);
 		if (wrt == -1)
                         return -1;
+		close(fd3);
 	}
 	close(fd);
 	return (1);
