9045fed3fa3d91ecb5348c8200fcddc5adc2a76c
walebign3 <walebign3@gmail.com>
2022-06-01 15:45:51 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,22 +30,19 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		close(fd);
 	}
-	else if (fd == -1 && text_content == NULL)
+	if (fd == -1 && text_content == NULL)
 	{
 		fd = open(filename, O_CREAT | O_RDWR, 0600);
 		if (fd == -1)
 			return (-1);
 		close(fd);
 	}
-	else
-	{
-		fd = open(filename, O_RDWR | O_TRUNC);
-		if (fd == -1)
-			return (-1);
-		wrt = write(fd, text_content, i);
-		if (wrt == -1)
-			return (-1);
-		close(fd);
-	}
+	fd = open(filename, O_RDWR | O_TRUNC);
+	if (fd == -1)
+		return (-1);
+	wrt = write(fd, text_content, i);
+	if (wrt == -1)
+		return (-1);
+	close(fd);
 	return (1);
 }
