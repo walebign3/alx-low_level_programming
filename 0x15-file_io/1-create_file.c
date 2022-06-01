ed50c1465abf6342a3bce9af8fa0720409c26a18
walebign3 <walebign3@gmail.com>
2022-06-01 15:38:45 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,10 +41,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		fd = open(filename, O_RDWR | O_TRUNC);
 		if (fd == -1)
-			return -1;
+			return (-1);
 		wrt = write(fd, text_content, i);
 		if (wrt == -1)
-			return -1;
+			return (-1);
 		close(fd);
 	}
 	return (1);
