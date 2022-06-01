8b1ab6f16d67e7b16968491a4239f4978cd412fa
walebign3 <walebign3@gmail.com>
2022-06-01 13:32:07 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,17 +9,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd;
+	int fd;
 	ssize_t i = 0;
 
-        if (filename == NULL)
-        {
-                return (-1);
-        }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 	while (*(text_content + i))
 		i++;
 	fd = open(filename, O_RDONLY);
-	if(fd == -1 && text_content != NULL)
+	if (fd == -1 && text_content != NULL)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | S_IRUSR | S_IWUSR);
 		write(fd, text_content, i);
@@ -27,9 +27,10 @@ int create_file(const char *filename, char *text_content)
 	else if (fd == -1 && text_content == NULL)
 		fd = open(filename, O_CREAT | S_IRUSR | S_IWUSR);
 	else
-	{	fd = open(filename, O_WRONLY | O_TRUNC);
+	{
+		fd = open(filename, O_WRONLY | O_TRUNC);
 		write(fd, text_content, i);
 	}
-	close (fd);
-        return (1);
+	close(fd);
+	return (1);
 }
