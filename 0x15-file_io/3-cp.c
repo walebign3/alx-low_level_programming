c07d9d679a2e44f2edb3db85e6c876c28456761c
walebign3 <walebign3@gmail.com>
2022-06-02 09:33:10 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,25 +20,30 @@ int main(int ac, char **av)
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
         {
-		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
         }
 	while ((rd = read(fd1, BUF, 1024)) > 0)
 		if (write(fd2, BUF, rd) != rd)
 		{
-			dprintf(2, "Error: Can't write to NAME_OF_THE_FILE\n");
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	if (rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	c1 = close(fd1);
 	c2 = close(fd2);
-	if (c1 == -1 || c2 == -1)
+	if (c1 == -1)
 	{
-		dprintf(2, "Error: Can't close fd FD_VALUE\n");
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (c2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
