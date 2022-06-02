9591ff66858c55f3ebf349daad35e65d1c81748a
walebign3 <walebign3@gmail.com>
2022-06-02 14:41:33 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,14 +14,8 @@ int main(int ac, char **av)
 {
 	int fd1, fd2, c1, c2;
 	ssize_t rd, wrt;
-	char *BUF;
+	char BUF[1024];
 
-	BUF = malloc(sizeof(char) * 1024);
-	if (BUF == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -34,20 +28,17 @@ int main(int ac, char **av)
 		if (fd1 == -1 || rd == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			free(BUF);
 			exit(98);
 		}
 		wrt = write(fd2, BUF, rd);
 		if (wrt == -1 || fd2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			free(BUF);
 			exit(99);
 		}
 		rd = read(fd1, BUF, 1024);
 		fd2 = open(av[2], O_WRONLY | O_APPEND);
 	} while (rd > 0);
-	free(BUF);
 	c1 = close(fd1);
 	c2 = close(fd2);
 	if (c1 == -1)
