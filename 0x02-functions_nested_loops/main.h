d6db0c1d772472461251d30891b859ed3fe4e30c
walebign3 <walebign3@gmail.com>
2022-06-16 23:52:18 +0300

    100-main.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 /* this prototype has to be declared in a header file */
-int _putchar(char n);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,3 +11,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
