0586b5a492c5b6d27fd0476567a4ff28ca0b45cc
walebign3 <walebign3@gmail.com>
2022-06-17 14:06:01 +0300

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer to first node
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *nd;
+
+	while (head)
+	{
+		nd = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
