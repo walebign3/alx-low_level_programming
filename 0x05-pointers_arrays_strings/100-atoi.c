d18e736fae5a146b52d33f4f96df72d8002475e7
walebign3 <walebign3@gmail.com>
2022-06-17 10:16:33 +0300

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+/**
+* _atoi - convert string to integer
+* @s: string
+*
+* Return: integer
+*/
+int _atoi(char *s)
+{
+	int n = 1;
+	unsigned int intr = 0;
+
+	do {
+		if (*s == '-')
+			n *= -1;
+		else if (*s >= '0' && *s <= '9')
+			intr = intr * 10 + (*s - '0');
+		else if (intr > 0)
+			break;
+	} while (*s++);
+	return (intr *n);
+}
