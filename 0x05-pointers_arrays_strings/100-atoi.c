1b2a5625751bafb9ce05a9ba1a4f17210ae4dcf5
walebign3 <walebign3@gmail.com>
2022-06-17 10:24:36 +0300

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,14 @@ int _atoi(char *s)
 	int n = 1;
 	unsigned int intr = 0;
 
-	do {
+	while (*s++)
+	{
 		if (*s == '-')
 			n *= -1;
 		else if (*s >= '0' && *s <= '9')
 			intr = intr * 10 + (*s - '0');
 		else if (intr > 0)
 			break;
-	} while (*s++);
+	}
 	return (intr *n);
 }
