7d61299a779f2bd71ab2a550dd97cb6c116da410
walebign3 <walebign3@gmail.com>
2022-07-07 11:04:11 +0300

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * print_dlistint - check the code
+ * @h: pointer to first node
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t l = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		l++;
+	}
+	return(l);
+}
