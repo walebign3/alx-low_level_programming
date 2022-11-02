e730f1315a1643ffcb09b9c15cb4b9729b364363
walebign3 <walebign3@gmail.com>
2022-11-02 11:14:11 +0300

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return i;
+	}
 	return -1;
 }
